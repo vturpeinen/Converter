fa5d55b731a24b13d6e23c0e452928dabe0685f1
vturpeinen <vturpeinen@hotmail.com>
2019-01-14 10:50:38 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+
+import 'package:flutter/material.dart';
+
+import 'category_route.dart';
+
+
+void main() {
+  runApp(UnitConverterApp());
+}
+
+/// This widget is the root of our application.
+///
+/// The first screen we see is a list [Categories], each of which
+/// has a list of [Unit]s.
+class UnitConverterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Kääntäjä',
+      theme: ThemeData(
+        fontFamily: 'Raleway',
+        textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: Colors.black,
+              displayColor: Colors.grey[600],
+            ),
+        // This colors the [InputOutlineBorder] when it is selected
+        primaryColor: Colors.grey[500],
+        textSelectionHandleColor: Colors.green[500],
+      ),
+      home: CategoryRoute(),
+    );
+  }
+}
